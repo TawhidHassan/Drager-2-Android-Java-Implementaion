aa5f8576730e9e2f278c2252b0e088ce9d17aeb8
sifat007 <sifat27.ht@gmail.com>
2020-11-07 17:42:53 +0600

    install dragger 2 by github google/drager

--- app/src/main/java/com/example/dragger2implementation/MainActivity.java
+++ app/src/main/java/com/example/dragger2implementation/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
